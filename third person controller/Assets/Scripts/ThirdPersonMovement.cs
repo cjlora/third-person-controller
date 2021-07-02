bd8791c8f929625305c9ce9a40ee55421b2ba083
cjlora <cjlora@gmail.com>
2021-07-02 18:54:29 +0800

    added move function

--- third person controller/Assets/Scripts/ThirdPersonMovement.cs
+++ third person controller/Assets/Scripts/ThirdPersonMovement.cs
@@ -14,6 +14,12 @@ public class ThirdPersonMovement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal)
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+        if(direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
