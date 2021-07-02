b84600a8b77cb2791ed74cfb2d347017a4c7a764
cjlora <cjlora@gmail.com>
2021-07-02 18:02:28 +0800

    added code in thirdpersonmovement (not done)

--- third person controller/Assets/Scripts/ThirdPersonMovement.cs
+++ third person controller/Assets/Scripts/ThirdPersonMovement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+
+    public float speed = 6f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal)
     }
 }
