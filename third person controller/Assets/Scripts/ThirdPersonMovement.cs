3323f3d956c8dd07bd6a182e8882712546cae1d3
cjlora <cjlora@gmail.com>
2021-07-02 19:17:49 +0800

    character looks toward where its moving

--- third person controller/Assets/Scripts/ThirdPersonMovement.cs
+++ third person controller/Assets/Scripts/ThirdPersonMovement.cs
@@ -19,6 +19,9 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
             controller.Move(direction * speed * Time.deltaTime);
         }
     }
